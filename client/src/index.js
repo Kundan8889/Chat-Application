8912bf3c8e799285954bd1ff0a7062824e66d391
Kundan Rathore <kundanrathore450@gmail.com>
2024-09-04 01:50:05 +0530

    update

--- client/src/index.js
+++ client/src/index.js
@@ -4,24 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
-import router from './rotues';
-import { Provider } from 'react-redux'
+import router from './rotues/index'; 
+import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router}>
-        <App />
-      </RouterProvider>
-    </Provider>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root'); // Ensure this matches the ID in your HTML
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <RouterProvider router={router}>
+          <App />
+        </RouterProvider>
+      </Provider>
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found. Ensure there is a div with id='root' in your index.html");
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
