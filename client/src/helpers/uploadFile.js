9ab01effd20cf460bfeb3ee4a4553fb3f29d017e
Kundan8889 <Kundan8889@users.noreply.github.com>
2026-01-11 18:21:46 +0530

    fix: cloudinary preset name

--- client/src/helpers/uploadFile.js
+++ client/src/helpers/uploadFile.js
@@ -1,23 +1,22 @@
-// helpers/uploadFile.js
-// import './style.css'
 const uploadFile = async (file) => {
-    const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/auto/upload`;
+  const cloudName = process.env.REACT_APP_CLOUDINARY_CLOUD_NAME;
 
-    const formData = new FormData();
-    formData.append('file', file);
-    formData.append("upload_preset", "chat-app-file");
+  const url = `https://api.cloudinary.com/v1_1/${cloudName}/auto/upload`;
 
-    const response = await fetch(url, {
-        method: 'POST',
-        body: formData
-    });
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", "chat-app-upload");
 
-    if (!response.ok) {
-        throw new Error('Failed to upload file');
-    }
+  const response = await fetch(url, {
+    method: "POST",
+    body: formData,
+  });
 
-    const responseData = await response.json();
-    return responseData;
+  if (!response.ok) {
+    throw new Error("Failed to upload file");
+  }
+
+  return response.json();
 };
 
 export default uploadFile;
